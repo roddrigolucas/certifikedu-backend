198e5ca05d9b892410342f9e478d7ddb03ccd0ba
roddrigolucas <roddrigolucas@users.noreply.github.com>
2026-07-08 11:10:01 -0300

    fix: casting enums for raw query

--- import-users.js
+++ import-users.js
@@ -9,7 +9,7 @@ async function main() {
   for (const user of data.users) {
     await prisma.$executeRawUnsafe(`
       INSERT INTO "users" (id, "createdAt", "updatedAt", email, "numeroDocumento", type, status, "tempName", "freeCertificates", "freeEvents", "apiEnabled", "ltiEnabled", "fromCanvas")
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+      VALUES ($1, $2, $3, $4, $5, $6::"UserType", $7::"UserStatus", $8, $9, $10, $11, $12, $13)
       ON CONFLICT (email) DO NOTHING;
     `, user.id, new Date(user.createdAt), new Date(user.updatedAt), user.email, user.numeroDocumento, user.type, user.status, user.tempName, user.freeCertificates, user.freeEvents, user.apiEnabled, user.ltiEnabled, user.fromCanvas);
   }
